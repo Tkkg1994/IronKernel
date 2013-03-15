5b779f74039eb2c94ac71e3203a9ff76d03ff736
Nai Xia <nai.xia@gmail.com>
2013-03-15 14:54:24 +0800

    anon_vma_unlock to anon_vma_unlock_read in v3.8
    Signed-off-by: Tkkg1994 <lg@linux.com>

--- mm/uksm.c
+++ mm/uksm.c
@@ -4675,7 +4675,7 @@ again:
 			struct anon_vma_chain *vmac;
 			struct vm_area_struct *vma;
 
-			anon_vma_lock(anon_vma);
+			anon_vma_lock_read(anon_vma);
 			anon_vma_interval_tree_foreach(vmac, &anon_vma->rb_root,
 						       0, ULONG_MAX) {
 
@@ -4707,7 +4707,7 @@ again:
 					break;
 			}
 
-			anon_vma_unlock(anon_vma);
+			anon_vma_unlock_read(anon_vma);
 			if (!mapcount)
 				goto out;
 		}
@@ -4742,7 +4742,7 @@ again:
 			struct anon_vma_chain *vmac;
 			struct vm_area_struct *vma;
 
-			anon_vma_lock(anon_vma);
+			anon_vma_lock_read(anon_vma);
 			anon_vma_interval_tree_foreach(vmac, &anon_vma->rb_root,
 						       0, ULONG_MAX) {
 				vma = vmac->vma;
@@ -4765,11 +4765,11 @@ again:
 				ret = try_to_unmap_one(page, vma,
 						       address, flags);
 				if (ret != SWAP_AGAIN || !page_mapped(page)) {
-					anon_vma_unlock(anon_vma);
+					anon_vma_unlock_read(anon_vma);
 					goto out;
 				}
 			}
-			anon_vma_unlock(anon_vma);
+			anon_vma_unlock_read(anon_vma);
 		}
 	}
 	if (!search_new_forks++)
@@ -4804,7 +4804,7 @@ again:
 			struct anon_vma_chain *vmac;
 			struct vm_area_struct *vma;
 
-			anon_vma_lock(anon_vma);
+			anon_vma_lock_read(anon_vma);
 			anon_vma_interval_tree_foreach(vmac, &anon_vma->rb_root,
 						       0, ULONG_MAX) {
 				vma = vmac->vma;
@@ -4820,11 +4820,11 @@ again:
 
 				ret = rmap_one(page, vma, address, arg);
 				if (ret != SWAP_AGAIN) {
-					anon_vma_unlock(anon_vma);
+					anon_vma_unlock_read(anon_vma);
 					goto out;
 				}
 			}
-			anon_vma_unlock(anon_vma);
+			anon_vma_unlock_read(anon_vma);
 		}
 	}
 	if (!search_new_forks++)
