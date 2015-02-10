ff36b766eb194d7b1c67573074fdc36c6bb06887
Tkkg1994 <luca.grifo@outlook.com>
2015-02-11 00:26:20 +0100

    fix governor issue

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 0 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Tue Feb 10 18:56:13 CET 2015"
+#define UTS_VERSION "#0 SMP PREEMPT Wed Feb 11 00:13:52 CET 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
 #define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
