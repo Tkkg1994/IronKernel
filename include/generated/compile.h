76ae2d8edcaa9efc1ab550e1e818a277ab0a7260
Tkkg1994 <luca.grifo@outlook.com>
2015-02-12 20:33:46 +0100

    Add Intelli-Plug Hotplug and try to fix OC to 2.2 GHz

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#10 SMP PREEMPT Thu Feb 12 14:25:33 CET 2015"
+#define UTS_VERSION "#5 SMP PREEMPT Thu Feb 12 20:25:46 CET 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
 #define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.106-IronKernel_Beta-gf314690-dirty"
+#define UTS_RELEASE "3.4.106-IronKernel_Beta-g818f835-dirty"
