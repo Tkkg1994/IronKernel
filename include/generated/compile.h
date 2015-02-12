723de941f5ae4abdf82913eff5fef7dd4213a7b2
Tkkg1994 <luca.grifo@outlook.com>
2015-02-12 03:23:12 +0100

    Fix OC and UC and fix performance governor fault
    	.config
    	include/config/auto.conf
    	include/config/kernel.release
    	include/config/tristate.conf
    	include/generated/autoconf.h
    	include/generated/compile.h
    	include/generated/utsrelease.h

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 SMP PREEMPT Thu Feb 12 02:37:54 CET 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
+#define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
