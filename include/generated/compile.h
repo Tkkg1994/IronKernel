cef28ec575c6fc9206cef0dddf7b14a639f6d612
Tkkg1994 <luca.grifo@outlook.com>
2015-02-13 01:27:39 +0100

    Enable Auto-Hotplug and revert OC to 2.2GHz

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Thu Feb 12 20:25:46 CET 2015"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Feb 13 01:26:28 CET 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
 #define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
