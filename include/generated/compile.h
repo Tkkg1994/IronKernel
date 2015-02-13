e41598d39d3b3df24d0dd296560ea8156c712d1e
Tkkg1994 <luca.grifo@outlook.com>
2015-02-13 20:44:05 +0100

    Revert row scheduler

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Feb 13 03:41:54 CET 2015"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Feb 13 20:42:23 CET 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
 #define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.106-IronKernel_Beta3-gcef28ec-dirty"
+#define UTS_RELEASE "3.4.106-IronKernel_Beta3-g268d095-dirty"
