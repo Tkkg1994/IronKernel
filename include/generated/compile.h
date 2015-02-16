b4bec1a66e4f77fea5bcb0601c465ee559ea0416
Tkkg1994 <luca.grifo@outlook.com>
2015-02-16 04:45:57 +0100

    Finally got everything back working! Revert minimums and more

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 13 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 SMP PREEMPT Mon Feb 16 02:58:58 CET 2015"
+#define UTS_VERSION "#13 SMP PREEMPT Mon Feb 16 04:21:00 CET 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
 #define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.106-IronKernel_Beta3-g5152dcb-dirty"
+#define UTS_RELEASE "3.4.106-IronKernel_Beta3-gace7abf-dirty"
