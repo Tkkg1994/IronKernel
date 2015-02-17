c8cda575f2692167a42559fe6606467144f46c74
Tkkg1994 <luca.grifo@outlook.com>
2015-02-17 20:19:34 +0100

    Try to fix problem with not booting, probably problem with earlysuspend and powersuspend

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 SMP PREEMPT Tue Feb 17 18:05:13 CET 2015"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Feb 17 19:57:21 CET 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
 #define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.106-IronKernel_Beta3-gf62f7fb-dirty"
+#define UTS_RELEASE "3.4.106-IronKernel_Beta3-g8bad66c-dirty"
