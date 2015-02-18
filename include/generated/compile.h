48efbf81b5af6311e50fa2c2ceb9e80489352553
Tkkg1994 <luca.grifo@outlook.com>
2015-02-18 19:46:52 +0100

    Enabled F2FS Support, thanks to Faux123 and thanks to halaszk to config it for the exynos source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Wed Feb 18 19:38:49 CET 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
+#define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.106-IronKernel_Beta4_TW_T805IronKernel_Beta4_TW_T805"
