cc207118e5801edb09538849619558588622836b
Tkkg1994 <luca.grifo@outlook.com>
2015-02-19 23:08:34 +0100

    Added sioplus sched to configs and set selinux to permessive

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 0 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Thu Feb 19 21:35:26 CET 2015"
+#define UTS_VERSION "#0 SMP PREEMPT Thu Feb 19 22:57:20 CET 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
 #define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
