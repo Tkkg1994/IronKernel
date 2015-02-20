0623ebb0f83630d97e10ff1d608c6a599ed8650c
Tkkg1994 <luca.grifo@outlook.com>
2015-02-20 14:24:08 +0100

    Changed Configs -> governors where there two times Deleted some unused stuff

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Thu Feb 19 22:57:20 CET 2015"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "linux-HP-Pavilion-dm4-Notebook-PC"
-#define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.4.106-IronKernel_Beta3_TW_T805"
