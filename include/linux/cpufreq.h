d744101ee3fda62635855e87053823a51c3aa936
Tkkg1994 <luca.grifo@outlook.com>
2015-02-25 18:18:13 +0100

    Samsung DYNAMIC Hotplug control thanks to halaskz

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -101,8 +101,11 @@ struct cpufreq_policy {
 	unsigned int		max;    /* in kHz */
 	unsigned int		cur;    /* in kHz, only needed if cpufreq
 					 * governors are used */
-	unsigned int            util;  /* CPU utilization at max frequency */
+	unsigned int            util;  	/* CPU utilization at max frequency */
 	unsigned int		policy; /* see above */
+#ifdef CONFIG_SEC_PM
+	unsigned int 		load_at_max; 	/* CPU utilization at max frequency */
+#endif
 	struct cpufreq_governor	*governor; /* see below */
 
 	struct work_struct	update; /* if update_policy() needs to be
